856eb994955e976883b54aae6cc40c97cc5a4638
suraj.kurde <svkurde03@gmail.com>
2022-07-24 21:41:00 +0530

    First Commit

--- Java/DSJava/src/first.java
+++ Java/DSJava/src/first.java
@@ -1,7 +1,58 @@
+import java.util.*;
+import java.io.*;
+
 public class first {
     public static void main(String[] args) {
-        System.out.println("Hello");
-        int intVar=10;
-        System.out.println(intVar);
+
+        //variable declaration
+        String firstName; // first + name
+        int rollNumber=0;   // declaration + initialization
+        String office_address;
+//        int 9id; incorrect declaration
+        int mobileNumber;
+        String _var123;
+        String var_1,var_2,var_3;
+
+
+        mobileNumber = 999999999;
+        char grade;
+        grade = 'A';
+
+
+        int[] mobile={1,2,3,4,5,6,7,8,9,10,11,12,13};
+        float pi=09f;
+        double pid=3.1478123414781234;
+
+        System.out.println("Value of pi: "+ pi);
+
+
+//
+       String fullName;
+        short age;
+//        char / String gender;
+//        int / long adharNumber;
+//        String address;
+        float salary;
+
+//        Read input
+
+//        Scanner class used to read user input
+        Scanner sc = new Scanner(System.in);//sc is object of Scanner class, used to read user input from standard input device
+        System.out.println("Enter full name");
+        fullName = sc.next();//string
+        System.out.println("Enter age");
+        age = sc.nextShort();//short type input
+        System.out.print("Enter salary" );
+        salary = sc.nextFloat();//input is of float type
+        int var = sc.nextInt();//input is of Integer type
+
     }
+}
+
+class second{
+
+}
+
+class third{
+
 }
